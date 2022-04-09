214dc95ed893af1494bb0693803961124a8669ec
Your Name <you@example.com>
2022-04-09 17:32:26 -0400

    update templete name

--- Project2/code/Cache.h
+++ Project2/code/Cache.h
@@ -10,11 +10,11 @@
 /*
  * SZ_TAG_BITS - How many bits from address are tag bits
  * SZ_DATA_BITS - How many bits do data fields have
- * SZ_BLOCK_SIZE - How many data fields (block) does one
+ * SZ_SET_ASSO - How many data fields (block) does one
  */
-template<size_t SZ_TAG_BITS, size_t SZ_DATA_BYTES, size_t SZ_BLOCK_SIZE, size_t SZ_NUM_BLOCK>
+template<size_t SZ_TAG_BITS, size_t SZ_BLOCK_SIZE, size_t SZ_SET_ASSO, size_t SZ_NUM_BLOCK>
 class _CACHE {
-    using _CACHE_BLOCK = std::array<_DATA_BLOCK<SZ_TAG_BITS, SZ_DATA_BYTES>, SZ_BLOCK_SIZE>;
+    using _CACHE_BLOCK = std::array<_DATA_BLOCK<SZ_TAG_BITS, SZ_BLOCK_SIZE>, SZ_SET_ASSO>;
     using _CACHE_ARRAY = std::array<_CACHE_BLOCK, SZ_NUM_BLOCK>;
     using _PARTITION = std::tuple<size_t, size_t, size_t>;
 private:
@@ -26,13 +26,13 @@ private:
 
 public:
     explicit _CACHE(unsigned int l = 0, _CACHE *p = nullptr, _CACHE *c = nullptr) : latency(l), parent(p), child(c) {
-        if (SZ_DATA_BYTES % 8 != 0)
-            throw std::logic_error("SZ_BLOCK_SIZE is not in BYTES.");
+        if (SZ_BLOCK_SIZE % 8 != 0)
+            throw std::logic_error("SZ_SET_ASSO is not in BYTES.");
         std::get<0>(this->field_partition) = SZ_TAG_BITS;
-        std::get<2>(this->field_partition) = round(log2(SZ_BLOCK_SIZE));
+        std::get<2>(this->field_partition) = round(log2(SZ_SET_ASSO));
         std::get<1>(this->field_partition) = round(log2(SZ_NUM_BLOCK));
         if (SZ_TAG_BITS != (32 - std::get<2>(this->field_partition) - std::get<1>(this->field_partition)))
-            throw std::logic_error("SZ_TAG_BITS doesn't match SZ_BLOCK_SIZE and SZ_NUM_BLOCK");
+            throw std::logic_error("SZ_TAG_BITS doesn't match SZ_SET_ASSO and SZ_NUM_BLOCK");
     }
 
     bool read_data(const Address &address) {
--- Project2/code/DataBlock.h
+++ Project2/code/DataBlock.h
@@ -7,18 +7,18 @@
 
 #include "Include.h"
 
-template<size_t SZ_TAG_BITS, size_t SZ_DATA_BYTES>
+template<size_t SZ_TAG_BITS, size_t SZ_BLOCK_SIZE>
 class _DATA_BLOCK {
 private:
     bool valid;
     uint32_t tag;
-    uint32_t data;
+    std::array<uint8_t, SZ_BLOCK_SIZE> data;
     std::chrono::time_point<std::chrono::steady_clock> last_use;
 public:
     explicit _DATA_BLOCK(bool valid = false, uint32_t tag = 0, uint32_t data = 0) {
         if (uint32_t(round(log2(tag))) > SZ_TAG_BITS) throw std::overflow_error("SZ_TAG_BITS could not fit TAG");
-        if (uint32_t(round(log2(data))) > SZ_DATA_BYTES) throw std::overflow_error("SZ_DATA_BITS could not fit DATA");
-        last_use = std::chrono::steady_clock::now();
+        if (uint32_t(round(log2(data))) > SZ_BLOCK_SIZE) throw std::overflow_error("SZ_DATA_BITS could not fit DATA");
+        this->last_use = std::chrono::steady_clock::now();
         this->valid = valid;
         this->tag = tag;
         this->data = data;
@@ -32,17 +32,16 @@ public:
         return this->tag;
     }
 
-    uint32_t getData() {
+    uint32_t getData(uint32_t offset) {
         this->last_use = std::chrono::steady_clock::now();
-        return this->data;
+        return this->data.at(offset);
     }
 
-    void setTagAndData(uint32_t tag, uint32_t data) {
+    void setTagAndData(uint32_t tag, const std::array<uint8_t, SZ_BLOCK_SIZE> &data) {
         if (uint32_t(round(log2(tag))) > SZ_TAG_BITS) throw std::overflow_error("SZ_TAG_BITS could not fit TAG");
-        if (uint32_t(round(log2(data))) > SZ_DATA_BYTES) throw std::overflow_error("SZ_DATA_BITS could not fit DATA");
         this->last_use = std::chrono::steady_clock::now();
         this->tag = tag;
-        this->data = data;
+        std::copy(data.begin(), data.end(), this->data.begin());
     }
 
 
